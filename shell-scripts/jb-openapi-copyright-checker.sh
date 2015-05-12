e854d5552a49efc7f4f86cea3b31082cfbdb3207
Vladislav Rassokhin <Vladislav.Rassokhin@jetbrains.com>
2015-05-12 22:09:46 +0300

    use current year

--- shell-scripts/jb-openapi-copyright-checker.sh
+++ shell-scripts/jb-openapi-copyright-checker.sh
@@ -3,13 +3,14 @@
 [ -z "$1" ] && echo "Specify directory to check" && exit 1
 
 directory="$1"
+this_year="$(date +'%Y')"
 
 for i in $(find $directory -type f -name '*.java' | sed -n "s|^${directory}/||p"); do
 	[ -z "$i" ] && continue
 	if [ ! -f $i ]; then
 		echo "Cannot acces file '$i'"
 	elif head -2 $i | tail -1 | grep -q 'JetBrains'; then
-		head -2 $i | tail -1 | grep -q '2000-2015' || echo -e "\e[33mOutdated copyright in file '$i':\t\t$(head -2 $i|tail -1)\e[0m";
+		head -2 $i | tail -1 | grep -q "2000-${this_year}" || echo -e "\e[33mOutdated copyright in file '$i':\t\t$(head -2 $i|tail -1)\e[0m";
 	else
 		echo -e "\e[31mNo copyright in file '$i'\e[0m";
 	fi
