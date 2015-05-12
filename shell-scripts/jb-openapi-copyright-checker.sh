ed72b0bc42eb452523c666f0f2f7ef2009762a5e
Vladislav Rassokhin <Vladislav.Rassokhin@jetbrains.com>
2015-05-12 22:07:26 +0300

    Add jb-openapi-copyright-checker.sh

---
+++ shell-scripts/jb-openapi-copyright-checker.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+[ -z "$1" ] && echo "Specify directory to check" && exit 1
+
+directory="$1"
+
+for i in $(find $directory -type f -name '*.java' | sed -n "s|^${directory}/||p"); do
+	[ -z "$i" ] && continue
+	if [ ! -f $i ]; then
+		echo "Cannot acces file '$i'"
+	elif head -2 $i | tail -1 | grep -q 'JetBrains'; then
+		head -2 $i | tail -1 | grep -q '2000-2015' || echo -e "\e[33mOutdated copyright in file '$i':\t\t$(head -2 $i|tail -1)\e[0m";
+	else
+		echo -e "\e[31mNo copyright in file '$i'\e[0m";
+	fi
+done
+
