5bcd990e1063da374045c4fffb1695000f8064a0
Vladislav Rassokhin <Vladislav.Rassokhin@jetbrains.com>
2016-07-14 19:40:43 +0300

    Improve teamcity-remove.sh a bit

--- shell-scripts/teamcity-remove.sh
+++ shell-scripts/teamcity-remove.sh
@@ -3,5 +3,5 @@ if [ ! -d "$1" ]; then
 	echo "No such directory $1"
 	exit 1
 fi
-pushd "$1" && rm -r $(ls | grep -v 'data'| grep -v 'logs' ) && popd
+pushd "$1" && rm -rf buildAgent && rm -r $(ls | grep -v 'data'| grep -v 'logs' ) && popd
 
