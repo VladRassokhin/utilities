b0f28275aed419d78a16c88562797e55826a92cc
Vladislav Rassokhin <vladislav.rassokhin@jetbrains.com>
2016-09-21 14:28:57 +0200

    Terraform schemas exporter should support Terraform 0.7.0 and newer

--- shell-scripts/export-terraform-schemas.sh
+++ shell-scripts/export-terraform-schemas.sh
@@ -4,18 +4,25 @@ rm -rf 'out/'
 mkdir -p 'out/providers'
 mkdir -p 'out/provisioners'
 
-ls bin | grep -- '-provider-' | awk -F '-' '{$1=$2=""; print substr($0, 3)}' | sed 's/ /-/'> 'out/providers.list'
-ls bin | grep -- '-provisioner-' | awk -F '-' '{$1=$2=""; print substr($0, 3)}' | sed 's/ /-/'> 'out/provisioners.list'
+# Since 0.7.0 all plugins compiled into main binary, so let's just check sources
+ls builtin/providers > 'out/providers.list'
+ls builtin/provisioners > 'out/provisioners.list'
+
+# Pre 0.7.0 search for providers and provisioners
+#ls bin | grep -- '-provider-' | awk -F '-' '{$1=$2=""; print substr($0, 3)}' | sed 's/ /-/'> 'out/providers.list'
+#ls bin | grep -- '-provisioner-' | awk -F '-' '{$1=$2=""; print substr($0, 3)}' | sed 's/ /-/'> 'out/provisioners.list'
 
 
 echo "Exporting providers"
 for i in $(cat 'out/providers.list'); do
-    ./bin/terraform schemas -json -indent -type provider "$i" > "out/providers/$i.json";
+	echo "	$i"
+	./bin/terraform schemas -json -indent -type provider "$i" > "out/providers/$i.json";
 done
 
 echo "Exporting provisioners"
 for i in $(cat 'out/provisioners.list'); do
-    ./bin/terraform schemas -json -indent -type provisioner "$i" > "out/provisioners/$i.json";
+	echo "	$i"
+	./bin/terraform schemas -json -indent -type provisioner "$i" > "out/provisioners/$i.json";
 done
 
 echo "Exporting functions"
