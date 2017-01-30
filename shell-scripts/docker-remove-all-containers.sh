f1f218db4cee3869f4b9a7664a83f91726ab28d5
Vladislav Rassokhin <vladislav.rassokhin@jetbrains.com>
2017-01-30 17:44:53 +0300

    Improve docker-remove-all-containers.sh

--- shell-scripts/docker-remove-all-containers.sh
+++ shell-scripts/docker-remove-all-containers.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-docker ps -a | awk '{print $1}' | tail -n +2 | xargs -n 1 docker rm
+if [ $# > 0 ]; then
+	args=$*
+else
+	args='-a'
+fi
+
+docker ps $args | awk '{print $1}' | tail -n +2 | xargs -n 1 docker rm
