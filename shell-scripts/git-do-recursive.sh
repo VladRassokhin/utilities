405e6e80ddbc61dd2e7c5ba4e1fc72f98319a284
Vladislav Rassokhin <vladislav.rassokhin@jetbrains.com>
2018-03-29 19:23:46 +0300

    Add git-do-recursive.sh

---
+++ shell-scripts/git-do-recursive.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+dest=${1:-.}
+deep=${2:-5}
+shift
+shift
+echo "Performing operations on git repos in directory $dest with maxdepth $deep";
+find "$dest" -maxdepth "$deep" -type d -name '.git' -exec bash -c "cd \"{}/..\" && echo \"Under \$(pwd)\" && $*" \;
