afb4bbd89f3be593fdda649c90db30ccea99ed58
Vladislav Rassokhin <vladislav.rassokhin@jetbrains.com>
2018-03-29 19:24:52 +0300

    vault-setup.sh: do not override VAULT_ADDR

--- shell-scripts/vault-setup.sh
+++ shell-scripts/vault-setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-export VAULT_ADDR='http://127.0.0.1:8200'
+
+[ -z "$VAULT_ADDR" ] && export VAULT_ADDR='http://127.0.0.1:8200'
 
 vault auth-enable approle
 vault write auth/approle/role/testrole secret_id_ttl=10m token_num_uses=10 token_ttl=30s token_max_ttl=30m secret_id_num_uses=40
