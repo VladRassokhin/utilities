3e366a5bbae81e08ee5d9b28ac74a1b2fa27dc1c
Vladislav Rassokhin <vladislav.rassokhin@jetbrains.com>
2018-04-10 23:48:33 +0300

    Minor in git-do-recursive.sh

--- shell-scripts/git-do-recursive.sh
+++ shell-scripts/git-do-recursive.sh
@@ -4,4 +4,4 @@ deep=${2:-5}
 shift
 shift
 echo "Performing operations on git repos in directory $dest with maxdepth $deep";
-find "$dest" -maxdepth "$deep" -type d -name '.git' -exec bash -c "cd \"{}/..\" && echo \"Under \$(pwd)\" && $*" \;
+find "$dest" -maxdepth "$deep" -type d -name '.git' -exec bash -c "cd '{}/..' && echo \"Performing '$*' in \$(pwd)\" && $*" \;
