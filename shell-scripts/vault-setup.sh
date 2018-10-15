e204b46142f25b4a074567caeb148beee1cdb4f5
Vladislav Rassokhin <vladislav.rassokhin@jetbrains.com>
2018-10-16 01:17:33 +0300

    Migrate to Vault KV2

--- shell-scripts/vault-setup.sh
+++ shell-scripts/vault-setup.sh
@@ -11,7 +11,7 @@ EOF
 vault write sys/policy/read-aws rules=@vault-simple-policy.hcl
 rm vault-simple-policy.hcl
 
-vault auth-enable approle
+vault auth enable approle
 
 echo "First approle"
 vault write auth/approle/role/testrole secret_id_ttl=10m token_num_uses=10 token_ttl=30s token_max_ttl=30m secret_id_num_uses=40 policies=read-aws
@@ -24,8 +24,8 @@ vault read auth/approle/role/secondrole/role-id
 vault write -f auth/approle/role/secondrole/secret-id
 
 
-vault write /secret/test data=simple
-vault write /secret/test-complex first=a second=2
+vault kv put /secret/test data=simple
+vault kv put /secret/test-complex first=a second=2
 
 
 
