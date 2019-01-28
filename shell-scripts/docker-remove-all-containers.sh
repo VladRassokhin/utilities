3c089b3bcfc8296530897f04d25f0166cb8c52ca
Vladislav Rassokhin <vladislav.rassokhin@jetbrains.com>
2019-01-28 09:11:53 +0300

    Minor fix in shell-scripts/docker-remove-all-containers.sh

--- shell-scripts/docker-remove-all-containers.sh
+++ shell-scripts/docker-remove-all-containers.sh
@@ -4,4 +4,4 @@ if [[ $# -gt 0 ]]; then
 	args=$*
 fi
 
-docker ps $args | awk '{print $1}' | tail -n +2 | xargs -n 1 -r docker rm
+docker ps $args | awk '{print $1}' | tail -n +2 | xargs -n 1 docker rm
