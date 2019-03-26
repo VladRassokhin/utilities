fcca47c94440b8bc686b6d68f1e0b01ab89ffec8
Vladislav Rassokhin <vladislav.rassokhin@jetbrains.com>
2019-03-26 14:09:03 +0300

    Improve git-fetch-recursive: parallel execution

--- shell-scripts/git-fetch-recursive.sh
+++ shell-scripts/git-fetch-recursive.sh
@@ -3,22 +3,23 @@
 while [[ $# -gt 0 ]]
 do
 case $1 in
-    -d|--maxdepth)
-    depth="$2"
-    shift
-    ;;
-    -p|--path)
-    directory="$2"
-    shift
-    ;;
-    *)
-    break
-    ;;
+    -d|--maxdepth) depth="$2"; shift ;;
+    -p|--path) directory="$2"; shift ;;
+    --parallel) parallel="$2"; shift ;;
+    *) break ;;
 esac
 shift
 done
 
 directory=${directory:-.}
 depth=${depth:-5}
+parallel=${parallel:-0}
 echo "Updating all git repos in directory $directory with maxdepth $depth";
-find "$directory" -maxdepth "$depth" -type d -name '.git' -exec bash -c "cd '{}' && echo 'Updating {}' && git fetch --all --prune" \;
+
+if [[ "$parallel" -gt 0 ]]; then
+	# TODO: Determine parallelness based on cpu count
+	find "$directory" -maxdepth "$depth" -type d -name '.git' -print0 | xargs -0 -P $parallel -I{} bash -c "cd '{}' && echo 'Updating {}' && git fetch --all --prune $@" \;
+else
+	find "$directory" -maxdepth "$depth" -type d -name '.git' -exec bash -c "cd '{}' && echo 'Updating {}' && git fetch --all --prune $@" \;
+fi
+
